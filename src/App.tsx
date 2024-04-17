280c50359efa87215e9cc243ea3be5d5672e5cd8
ofrancois <olivier.francois09@gmail.com>
2024-04-17 15:40:26 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className="flex flex-col">
+        <div className="p-4 rounded bg-cyan-500 w-32">Hello world</div>
+    </div>
+  )
+}
+
+export default App
