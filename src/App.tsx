4f5874555501c9dccf8e5cea44a4611293fc4845
ofrancois <olivier.francois09@gmail.com>
2024-04-17 15:45:42 +0200

    daisy

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="flex flex-col">
         <div className="p-4 rounded bg-cyan-500 w-32">Hello world</div>
+
+        <input type="text" className="input" />
     </div>
   )
 }
