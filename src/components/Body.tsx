26a349e78a73018e749616a267c9e91089a9877e
ofrancois <olivier.francois09@gmail.com>
2024-04-25 11:49:06 +0200

    wip

--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -9,7 +9,7 @@ export default function Body() {
     return <div className={'flex-1 flex flex-col overflow-hidden'}>
         <Header/>
 
-        <div className={'flex-1 grid grid-cols-2 max-h-full overflow-y-scroll'}>
+        <div className={`flex-1 grid grid-cols-${messages.length} max-h-full overflow-y-scroll`}>
             {messages.map((message, index) => (
                 <div key={index} className={'message-container p-1 flex-1'} id={`message-container-${index}`}>
                     <PrettyDisplay message={message}/>
